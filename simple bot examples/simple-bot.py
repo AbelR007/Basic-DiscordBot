299c1533557a76b3361f63d8314c136f2e9aaa5a
Abel Roy <abelroi007@gmail.com>
2021-07-07 12:17:50 +0530

    Update simple-bot.py

--- simple bot examples/simple-bot.py
+++ simple bot examples/simple-bot.py
@@ -1,1 +1,18 @@
-# Working...
+import discord
+from discord.ext import commands
+
+client = commands.Bot(command_prefix=">")
+
+@client.event
+async def on_ready():
+	print("Bot is running.")
+
+@client.command()
+async def hello(ctx):
+	await ctx.send(f"Hi there, {ctx.author.mention}")
+
+
+client.run("Bot_token_here")
+
+#====================================
+# CREATED with ❤️ by AbelR007
